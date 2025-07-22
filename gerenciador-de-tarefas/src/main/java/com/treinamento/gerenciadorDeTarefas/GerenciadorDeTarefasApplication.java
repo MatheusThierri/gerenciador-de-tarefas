fb773b0e3a7fd6e0f89015e3984cc9a5c1833995
MatheusThierri <matheusthierri7@gmail.com>
2025-07-21 22:51:23 -0300

    Criação da aplicação

---
+++ gerenciador-de-tarefas/src/main/java/com/treinamento/gerenciadorDeTarefas/GerenciadorDeTarefasApplication.java
@@ -0,0 +1,11 @@
+package com.treinamento.gerenciadorDeTarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDeTarefasApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDeTarefasApplication.class, args);
+	}
+}
